5880955eb13290eb367a7bb1c0846fd85d7478cf
Petar Petrov <petar.p.petrov@cchellenic.com>
2015-05-04 17:24:46 +0300

    Add DbTable definition

---
+++ AppBuilder/Db/DbTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppBuilder.Db
+{
+	public sealed class DbTable
+	{
+		public string Name { get; private set; }
+
+		public DbTable(string name)
+		{
+			if (name == null) throw new ArgumentNullException("name");
+
+			this.Name = name;
+		}
+	}
+}
